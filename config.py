0fc339799e86b56d7fda1f40042fca06d9587529
June Kim <junhwi.kimsc@gmail.com>
2021-07-31 15:19:33 -0400

    skeleton

---
+++ config.py
@@ -0,0 +1,65 @@
+from pathlib import Path
+from utils import get_datetime_version
+
+PROJECT_ROOT = Path(__file__).parent
+DATA_ROOT = PROJECT_ROOT.joinpath('data')
+
+# config
+metadata_config = {
+    "model_name": "baseline",
+    "version": get_datetime_version()
+}
+
+dataloader_config = {
+    # Set path variables for convenience
+    "data_path": DATA_ROOT.joinpath("train"),
+    "labels_path": DATA_ROOT.joinpath("training_labels.csv"),
+    "test_data_path": DATA_ROOT.joinpath("test"),
+    "test_labels_path": DATA_ROOT.joinpath("sample_submission.csv"),
+
+    "seed": 10,
+    "val_ratio": 0.2,
+    "batch_size": 64,
+    "num_workers": 0,
+}
+
+model_config = {
+    "resnet_pretrain": True,
+}
+
+policy_config = {
+    "loss_fn": "AUC",
+    "optimizer": "Adam",
+}
+
+logging_config = {
+    "use_wandb": True,
+    "name": "baseline",
+    "project": "g2net",
+    "entity": "dmlg"
+}
+
+trainer_config = {
+    "gpus": 1,
+    "auto_select_gpus": True,
+
+    "min_epochs": 0,
+    "max_epochs": 200,
+    "val_check_interval": 1000,
+
+    "resume_from_checkpoint": None,
+    "fast_dev_run": False,
+    "deterministic": False
+}
+
+checkpoint_config = {
+    "save_checkpoint": True,
+    "save_dir": PROJECT_ROOT.joinpath("checkpoints",
+                                        metadata_config["model_name"],
+                                        metadata_config["version"]),
+    "monitor": "val/loss",
+    "mode": "min",
+    "save_last": True,
+    "save_top_k": 3,
+    "every_n_train_steps": 100
+}
---
+++ model.py
@@ -0,0 +1,60 @@
+# model
+import pytorch_lightning as pl
+
+import torch
+from torch import nn
+from torch.nn import functional as F
+from torchvision import datasets, transforms
+from torchvision.models import resnet18
+
+from losses import roc_star_loss
+
+class LightningG2Net(pl.LightningModule):
+    def __init__(self, config):
+        super(LightningG2Net, self).__init__()
+        self.resnet = resnet18(pretrained=config['pretrained']) # remove last layer, fix first layer
+        self.output_layer = nn.Linear(1000, 2)
+
+        self.lr = config['lr']
+        self.optimizer_name = config['optimizer_name']
+        self.loss_fn = self.configure_loss_fn(config['loss_fn'])
+    
+    def configure_loss_fn(self, loss_fn):
+        if loss_fn == 'CrossEntropy':
+            return F.binary_cross_entropy
+        
+        elif loss_fn == 'ROC_Star':
+            return roc_star_loss
+        
+        else:
+            raise NotImplementedError(loss_fn)
+
+    def forward(self, x):
+        # resnet
+        x = self.resnet(x)
+        x = self.output_layer(x)
+        
+        # probability distribution over labels
+        x = torch.log_softmax(x, dim=1)
+        return x
+
+    def configure_optimizers(self):
+        if self.optimizer_name == 'Adam':
+            return torch.optim.Adam(self.parameters(), self.lr)
+
+        else:
+            raise NotImplementedError(self.optimizer_name)
+    
+    def training_step(self, batch, batch_idx):
+        x, y = batch
+        logits = self.forward(x)
+        loss = self.loss_fn(logits, y)
+        self.log('train_loss', loss)
+        return loss
+    
+    def validation_step(self, batch, batch_idx):
+        x, y = batch
+        logits = self.forward(x)
+        loss = self.loss_fn(y,logits,self.gamma)
+        self.log('val_loss', loss)
+        return loss
