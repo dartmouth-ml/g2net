27f09cb9ed941509fc47133589b1d5a8b7501413
Will Baxley <will.baxley27@gmail.com>
2021-07-31 17:50:56 -0400

    rudimentary dataloader is working!

--- dataloader.py
+++ dataloader.py
@@ -2,56 +2,39 @@ from torch.utils.data import Dataset, DataLoader
 import pandas as pd
 import numpy as np
 from pathlib import Path
-from librosa import power_to_db
-from librosa.feature import melspectrogram
-# from nnAudio.Spectrogram import CQT1992v2
 
-
-def make_spectrogram(time_series_data):
-    '''Creates a MEL spectrogram.'''
-    # Loop and make spectrogram
-    spectrograms = []
-
-    for i in range(3):
-        norm_data = time_series_data[i] / max(time_series_data[i])  # TODO is this the way we want to normalize?
-
-        # Compute a mel-scaled spectrogram.
-        spec = melspectrogram(norm_data, sr=4096, n_mels=128, fmin=20, fmax=2048)
-
-        # Convert a power spectrogram (amplitude squared) to decibel (dB) units
-        spec = power_to_db(spec).transpose((1, 0))
-        spectrograms.append(spec)
-
-    return np.stack(spectrograms)
+from spectrogram import make_spectrogram
 
 
 class SpectrogramDataset(Dataset):
-    def __init__(self, data_path, file_names, labels):
+    # data_path is a Path object
+    def __init__(self, data_path):
         super().__init__()
 
-        self.data_path = data_path
-        self.file_names = file_names
-        self.labels = labels
+        labels_df = pd.read_csv(data_path.joinpath('abbreviated_labels.csv'))
+
+        self.time_series_path = data_path.joinpath('train')
+        self.file_names = labels_df['id'].tolist()
+        self.labels = np.array(labels_df['target'].tolist())
 
         self.file_ext = '.npy'
 
     def __getitem__(self, idx):
         """
-        spectrogram -- [something] x [something] image
-        labels -- 0 or 1
+        spectrogram -- (currently) 9 x 128 color image
+        label -- 0 or 1
         """
-        file_name = self.file_names.iloc[idx]
+        file_name = self.file_names[idx]
         full_path = self.convert_to_full_path(file_name)
         time_series_data = np.load(full_path).astype(np.float32)
         spectrogram = make_spectrogram(time_series_data)
 
-        label = self.labels.iloc[idx]
-        label = "/".join(label.split("/")[1:])
+        label = self.labels[idx]
 
         return spectrogram, label, file_name
 
     def convert_to_full_path(self, file_name):
-        full_path = self.data_path.joinpath(*[s for s in file_name[:3]], file_name).with_suffix(self.file_ext)
+        full_path = self.time_series_path.joinpath(*[s for s in file_name[:3]], file_name).with_suffix(self.file_ext)
         assert full_path.is_file(), full_path
 
         return full_path
@@ -72,8 +55,15 @@ def make_dataloader(batch_size):
                       shuffle=True)
 
 
+# A little bit of testing
 if __name__ == '__main__':
-    test_series = np.load('data/train/7/7/7/777a1e4add.npy')
-    spectrogram = make_spectrogram(test_series)
-    print(type(spectrogram))
-    print(spectrogram.shape)
+    dset = SpectrogramDataset(Path.cwd().joinpath('data'))
+    for spectrogram, label, file_name in dset:
+        print(file_name)
+
+    spectrogram, label, file_name = dset.__getitem__(5)
+    print(label)
+    print(spectrogram)
+
+    # test_series = np.load('data/train/7/7/7/777a1e4add.npy')
+    # spectrogram = make_spectrogram(test_series)
