339a1bc848bd81a9ab617e8ebe93cf2d3f56718b
June Kim <junhwi.kimsc@gmail.com>
2021-08-03 15:26:56 -0400

    series

--- test.py
+++ test.py
@@ -32,8 +32,8 @@ def create_submission(model, trainer, datamodule):
     all_predictions = np.concatenate(all_predictions, axis=0)
 
     print(len(all_ids), all_predictions.shape)
-    submission.loc['id'] = all_ids
-    submission.loc['target'] = all_predictions
+    submission['id'] = pd.Series(all_ids)
+    submission['target'] = pd.Series(all_predictions)
 
     print(f'submission shape: {submission.shape}')
     submission.to_csv("submission.csv", index=False, index_label=False)
