ca25c5ba5f366422731d114e5c2a748a077f2eaa
Will Baxley <will.baxley27@gmail.com>
2021-09-09 22:24:29 -0400

    added sqrt transformation to CQT method

--- spectrogram.py
+++ spectrogram.py
@@ -13,7 +13,7 @@ def make_spectrogram(time_series_data):
     spectrograms = []
 
     for i in range(3):
-        norm_data = time_series_data[i] / max(time_series_data[i])  # TODO is this the way we want to normalize?
+        norm_data = time_series_data[i] / max(time_series_data[i])
 
         # Compute a mel-scaled spectrogram.
         spec = melspectrogram(norm_data, sr=4096, n_mels=128, fmin=20, fmax=2048)
@@ -25,30 +25,37 @@ def make_spectrogram(time_series_data):
     return torch.stack(spectrograms)
 
 
-# Generic mel spectrogram method. Takes in a variety of (optional) parameters.
-#   "time_series_data" - 3 x 2048 numpy array
-def spectrogram_mel(time_series_data, sr=4096, n_mels=128, fmin=20, fmax=2048):
+"""
+Generic mel spectrogram method. Takes in a variety of (optional) parameters.
+    time_series_data - 3 x 4096 numpy array
+    n_mels - the number of frequency bins
+    (sr)/(hop_length) = number of time bins
+"""
+def spectrogram_mel(time_series_data, sr=4096, n_mels=128, hop_length=500, fmin=20, fmax=2048):
     spectrograms = []
 
     for i in range(3):
         norm_data = time_series_data[i] / max(time_series_data[i])
 
         # Compute a mel-scaled spectrogram
-        spec = melspectrogram(norm_data, sr=sr, n_mels=n_mels, fmin=fmin, fmax=fmax)
+        spec = melspectrogram(norm_data, sr=sr, n_mels=n_mels, hop_length=hop_length, fmin=fmin, fmax=fmax)
 
         # Convert a power spectrogram (amplitude squared) to decibel (dB) units
-        spec = power_to_db(spec).transpose((1, 0))
-        spectrograms.append(torch.from_numpy(spec))
+        spec = power_to_db(spec)
+
+        spectrograms.append(torch.from_numpy(spec.copy()))
 
     return torch.stack(spectrograms)
 
 
+def spectrogram_CQT(time_series_data, sr=2048, hop_length=32, n_bins=None, fmin=20, fmax=1024, transformation=None):
+    if n_bins == None:
+        TRANSFORM = CQT1992v2(sr=sr, hop_length=hop_length, fmin=fmin, fmax=fmax, verbose=False)
 
-def spectrogram_CQT(time_series_data):
-    '''Transforms the np_file into spectrogram.'''
-    TRANSFORM = CQT1992v2(sr=2048, fmin=20,
-                          fmax=1024, hop_length=32,
-                          verbose=False)
+    # We have to get rid of the fmin and fmax parameters in order to use n_bins
+    else:
+        TRANSFORM = CQT1992v2(sr=sr, n_bins=n_bins, hop_length=hop_length, verbose=False)
+
 
     spectrograms = []
 
@@ -60,7 +67,16 @@ def spectrogram_CQT(time_series_data):
         channel = TRANSFORM(norm_data).squeeze()
         spectrograms.append(channel)
 
-    return torch.stack(spectrograms)
+    stack = torch.stack(spectrograms)
+
+    if transformation == 'sqrt':
+        stack = torch.sqrt(stack)
+
+    return stack
+
+
+def sqrt_CQT(time_series_data):
+    return spectrogram_CQT(time_series_data, transformation='sqrt')
 
 
 def vertical_image_concat(images, gap=2):
@@ -80,18 +96,21 @@ def vertical_image_concat(images, gap=2):
     return np.concatenate(pieces, axis=0)
 
 
-def visualize_spectrogram(spect, name='my_plot'):
+def visualize_spectrogram(spect, name='my_plot', foler = ''):
     images = []
 
     for site in range(3):
         # 1. get the site data and convert to numpy
         data = spect[site].detach().cpu().numpy()
 
-        # 2. rescale to [0, 1]
+        # 3. rescale to [0, 1]
         data = (data - data.min())
         data = data if data.max() == 0 else data / data.max()
 
-        # 3. convert to image
+        # 3. vertical flip
+        data = np.flip(data, axis=0)
+
+        # 4. convert to image
         h, w = data.shape
         img = np.zeros((h, w, 3))
         for y in range(h):
@@ -112,9 +131,22 @@ def visualize_spectrogram(spect, name='my_plot'):
     plt.savefig(f'{name}')
 
 
+""" archive """
+# inputA = np.load(Path.cwd().joinpath('data_full', 'data', '0', '0', '4', '004122364d.npy'))
+# inputB = np.load(Path.cwd().joinpath('data_full', 'data', '0', '3', '5', '035bfb20bc.npy'))
+# inputC = np.load(Path.cwd().joinpath('data_full', 'data', '1', '5', 'b', '15b2ec7e87.npy'))
+
+# spect = spectrogram_mel(inputA, n_mels=128, hop_length=500)
+# visualize_spectrogram(spect, 'mel_original')
+
+# spect = spectrogram_mel(inputA, n_mels=128, hop_length=32)
+# visualize_spectrogram(spect, 'mel_wide')
+
 if __name__ == '__main__':
-    inputA = np.load(Path.cwd().joinpath('data_full', '0', '0', '4', '004122364.npy'))
+    inputA = np.load(Path.cwd().joinpath('data_full', 'data', '0', '0', '4', '004122364d.npy'))
 
-    spect = spectrogram_mel(inputA)
+    spect = sqrt_CQT(inputA)
+    visualize_spectrogram(spect, 'CQT_sqrt')
 
-    visualize_spectrogram(spect, 'mel_)
+    # spect = spectrogram_mel(inputC, n_mels=128, hop_length=32)
+    # visualize_spectrogram(spect, 'mel_big')
